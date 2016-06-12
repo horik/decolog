7151cadd27c0854c60bea712c62178b07b0df447
horik <kotahori@horik.org>
2016-06-13 08:58:15 +0900

    initial commit

---
+++ decolog.py
@@ -0,0 +1,51 @@
+import requests
+import json
+import re
+from collections import OrderedDict
+from bs4 import BeautifulSoup
+
+
+def decolog(name):
+    """
+    :param name: str
+    :return: str json
+    """
+    html = requests.get("http://www.dclog.jp/%s" % name)
+    soup = BeautifulSoup(html.text, "html.parser")
+    data = []
+    for article in soup.select('article'):
+        entry_id = re.sub(r'^/en/[0-9]+/', '', article.header.a.get('href'))
+        title = article.header.text.replace('\n', '')
+        content = ''
+        images = []
+        created_at = article.time.get('datetime')
+        for div in article.select('.decoBody div'):
+            if div.img:
+                # 1つのdivの中に複数のimgがある場合の対策
+                for img in div.find_all('img'):
+                    if not images:
+                        # imagesが空の場合はそのまま追加する
+                        images.append(img.get('src'))
+                    else:
+                        # それ以外は重複チェックをしてから追加する
+                        has_no_duplicate = True
+                        for image in images:
+                            if image == img.get('src'):
+                                has_no_duplicate = False
+                        if has_no_duplicate:
+                            images.append(img.get('src'))
+            elif div.text == '':
+                content += '\n'
+            else:
+                content += div.text + '\n'
+        data.append(OrderedDict([
+            ('entry_id', entry_id),
+            ('title', title),
+            ('content', content),
+            ('images', images),
+            ('created_at', created_at)
+        ]))
+    json_str = json.dumps(data, ensure_ascii=False, indent=2)
+    return json_str
+
+# open('data.json', 'w').write(decolog('erijchan'))
